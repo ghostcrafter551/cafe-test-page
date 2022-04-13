af027ac14650788a794aca1e462ff326076ef8c7
ghostcrafter551 <62945074+ghostcrafter551@users.noreply.github.com>
2022-04-13 13:16:34 +0200

    Delete pages directory

--- pages/home.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function HomePage({}) {
-  return (
-    <main className={styles.container}>
-      <h1>Hello World</h1>
-    </main>
-  );
-}
