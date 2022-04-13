ea4fed1a7d4a40f2d20c40f62294ca4db4872f89
ghostcrafter551 <->
2022-04-13 13:18:00 +0200

    Updates

---
+++ server/chunks/553.js
@@ -0,0 +1,93 @@
+exports.id = 553;
+exports.ids = [553];
+exports.modules = {
+
+/***/ 2221:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"main": "NavBar_main__lVrG2",
+	"header": "NavBar_header__fk3A5",
+	"logo": "NavBar_logo__C2v21",
+	"nav_links": "NavBar_nav_links__P1aNg",
+	"nav_link": "NavBar_nav_link__K3f_4",
+	"apply_transition": "NavBar_apply_transition__jfyAp"
+};
+
+
+/***/ }),
+
+/***/ 8553:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ NavBar)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _NavBar_module_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(2221);
+/* harmony import */ var _NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var react_icons_cg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(7865);
+/* harmony import */ var react_icons_cg__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_icons_cg__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+function NavBar() {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("main", {
+        className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().main),
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+            className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().header),
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().apply_transition),
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_cg__WEBPACK_IMPORTED_MODULE_2__.CgCoffee, {
+                        className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().logo)
+                    })
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("nav", {
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                        className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().nav_links),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().nav_link),
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                                    href: "/home",
+                                    children: "Home"
+                                })
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().nav_link),
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                                    href: "/menu",
+                                    children: "Speisekarte"
+                                })
+                            })
+                        ]
+                    })
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().apply_transition),
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                        href: "/home",
+                        className: (_NavBar_module_css__WEBPACK_IMPORTED_MODULE_4___default().apply_transition),
+                        children: "Kontakt"
+                    })
+                })
+            ]
+        })
+    });
+};
+
+
+/***/ })
+
+};
+;
